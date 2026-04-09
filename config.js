9e63fe0c73c992506e52225ff5a7fb6d31a9fd2b
dullamanyama0-eng <dullamanyama0@gmail.com>
2026-04-09 11:17:46 +0300

    Update and rename server.js to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs-extra')
+
+global.owner = "255694670587"
+global.ownername = "Dvary MANYAMA"
+global.botname = "DVARY SECURITY V2"
+global.prefix = "."
+global.THUMB_IMAGE = "https://telegra.ph/file/54efddccf41281ad7ec51.jpg"
+
+module.exports = {
+  VERSION: "V2.0.0",
+  CAPTION: "©²⁰²⁶ ᴅᴠᴀʀʏ sᴇᴄᴜʀɪᴛʏ ᴠ2"
+}
